44ec3953c7f4fc85de0201ba1b3228a5fdd66888
Tuananhmaii <anhmttgcd201452@fpt.edu.vn>
2022-11-25 19:29:31 +0700

    Add Identity table

---
+++ App_Dev/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace App_Dev.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
