c7408d1f6d02fe24b6bc868a0971e6486dcf93b2
Tuananhmaii <anhmttgcd201452@fpt.edu.vn>
2022-12-09 20:40:05 +0700

    Done shopping cart

--- App_Dev/Controllers/CartController.cs
+++ App_Dev/Controllers/CartController.cs
@@ -35,5 +35,39 @@ namespace App_Dev.Controllers
             }
             return View(shoppingCartVM);
         }
+
+
+
+
+        public IActionResult Plus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+            _unitOfWork.ShoppingCart.IncrementCount(cart, 1);
+            _unitOfWork.Save();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Minus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+            if (cart.Count > 1)
+            {
+                _unitOfWork.ShoppingCart.DecrementCount(cart, 1);
+                var count = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == cart.ApplicationUserId).ToList().Count - 1;
+            }
+            else
+            {
+                _unitOfWork.ShoppingCart.Remove(cart);
+            }
+            _unitOfWork.Save();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Remove(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+            _unitOfWork.ShoppingCart.Remove(cart);
+            _unitOfWork.Save();
+            var count = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == cart.ApplicationUserId).ToList().Count;
+            return RedirectToAction("Index");
+        }
     }
 }
