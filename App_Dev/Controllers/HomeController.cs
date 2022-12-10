ffdb0eab20de197d2e9ba89d3cfdd94a6f87344b
Tuananhmaii <anhmttgcd201452@fpt.edu.vn>
2022-12-10 10:48:32 +0700

    Search Book function

--- App_Dev/Controllers/HomeController.cs
+++ App_Dev/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using App_Dev.Models;
+﻿using App_Dev.Data;
+using App_Dev.Models;
 using App_Dev.Repository.IRepository;
 using App_Dev.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Security.Claims;
@@ -14,15 +16,22 @@ namespace App_Dev.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IUnitOfWork _unitOfWork;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork, ApplicationDbContext db)
         {
             _logger = logger;
             _unitOfWork = unitOfWork;
+            _db = db;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string? search)
         {
+            if(search != null)
+            {
+                IEnumerable<Book> searchList = _db.Books.Where(u => u.Name.Contains(search) || u.Category.Name.Contains(search)).Include("Category");
+                return View(searchList);
+            }
             IEnumerable<Book> bookList = _unitOfWork.Book.GetAll(includeProperties: "Category");
             return View(bookList);
         }
