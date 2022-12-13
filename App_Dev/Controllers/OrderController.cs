86913148facb4c6a2c66b2736bf912957f2fded3
Tuananhmaii <anhmttgcd201452@fpt.edu.vn>
2022-12-13 15:01:34 +0700

    Customer's orders record

---
+++ App_Dev/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using App_Dev.Data;
+using App_Dev.Models;
+using App_Dev.Repository.IRepository;
+using App_Dev.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace App_Dev.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public OrderController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Order> orderList = _db.Orders;
+            return View(orderList);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int orderId)
+        {
+            OrderVM orderVM = new()
+            {
+                Order = _db.Orders.Find(orderId),
+                OrderItem = _db.OrderItems.Include("Book").Where(n => n.OrderId == orderId)
+            };
+            return View(orderVM);
+        }
+    }
+}
---
+++ App_Dev/ViewModels/OrderVM.cs
@@ -0,0 +1,10 @@
+﻿using App_Dev.Models;
+
+namespace App_Dev.ViewModels
+{
+    public class OrderVM
+    {
+        public Order Order { get; set; }
+        public IEnumerable<OrderItem> OrderItem { get; set; }
+    }
+}
